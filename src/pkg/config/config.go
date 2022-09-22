ddede054679fb91bc19415384bd7294ac4863817
Ori Shoshan <ori@otterize.com>
2022-09-22 14:48:56 +0300

    Change default mapper namespace to otterize-system (#4)

--- src/pkg/config/config.go
+++ src/pkg/config/config.go
@@ -9,7 +9,7 @@ const (
 	MapperServiceNameKey     = "mapper-service-name"
 	MapperServiceNameDefault = "otterize-network-mapper"
 	MapperNamespaceKey       = "mapper-namespace"
-	MapperNamespaceDefault   = "otterize"
+	MapperNamespaceDefault   = "otterize-system"
 	MapperServicePortKey     = "mapper-service-port"
 	MapperServicePortDefault = 9090
 )
