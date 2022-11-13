0b18bcc9bfc0a8fe0a52231709ef3af267a83d3a
amit7itz <amit.i@otterize.com>
2022-11-13 16:19:10 +0200

    Using the new graphql API and the "graphql" repo

--- src/pkg/users/generated.go
+++ src/pkg/users/generated.go
@@ -8,115 +8,131 @@ import (
 	"github.com/Khan/genqlient/graphql"
 )
 
-// CreateUserFromAuth0UserCreateUserFromAuth0User includes the requested fields of the GraphQL type User.
-type CreateUserFromAuth0UserCreateUserFromAuth0User struct {
-	Id            string                                                      `json:"id"`
-	Email         string                                                      `json:"email"`
-	Auth0UserId   string                                                      `json:"auth0UserId"`
-	Organization  CreateUserFromAuth0UserCreateUserFromAuth0UserOrganization  `json:"organization"`
-	Auth0UserInfo CreateUserFromAuth0UserCreateUserFromAuth0UserAuth0UserInfo `json:"auth0UserInfo"`
+// CreateUserFromAuth0UserMeMeMutation includes the requested fields of the GraphQL type MeMutation.
+type CreateUserFromAuth0UserMeMeMutation struct {
+	RegisterUser CreateUserFromAuth0UserMeMeMutationRegisterUser `json:"registerUser"`
 }
 
-// GetId returns CreateUserFromAuth0UserCreateUserFromAuth0User.Id, and is useful for accessing the field via an interface.
-func (v *CreateUserFromAuth0UserCreateUserFromAuth0User) GetId() string { return v.Id }
+// GetRegisterUser returns CreateUserFromAuth0UserMeMeMutation.RegisterUser, and is useful for accessing the field via an interface.
+func (v *CreateUserFromAuth0UserMeMeMutation) GetRegisterUser() CreateUserFromAuth0UserMeMeMutationRegisterUser {
+	return v.RegisterUser
+}
 
-// GetEmail returns CreateUserFromAuth0UserCreateUserFromAuth0User.Email, and is useful for accessing the field via an interface.
-func (v *CreateUserFromAuth0UserCreateUserFromAuth0User) GetEmail() string { return v.Email }
+// CreateUserFromAuth0UserMeMeMutationRegisterUser includes the requested fields of the GraphQL type User.
+type CreateUserFromAuth0UserMeMeMutationRegisterUser struct {
+	Id            string                                                       `json:"id"`
+	Email         string                                                       `json:"email"`
+	Auth0UserId   string                                                       `json:"auth0UserId"`
+	Organization  CreateUserFromAuth0UserMeMeMutationRegisterUserOrganization  `json:"organization"`
+	Auth0UserInfo CreateUserFromAuth0UserMeMeMutationRegisterUserAuth0UserInfo `json:"auth0UserInfo"`
+}
 
-// GetAuth0UserId returns CreateUserFromAuth0UserCreateUserFromAuth0User.Auth0UserId, and is useful for accessing the field via an interface.
-func (v *CreateUserFromAuth0UserCreateUserFromAuth0User) GetAuth0UserId() string {
+// GetId returns CreateUserFromAuth0UserMeMeMutationRegisterUser.Id, and is useful for accessing the field via an interface.
+func (v *CreateUserFromAuth0UserMeMeMutationRegisterUser) GetId() string { return v.Id }
+
+// GetEmail returns CreateUserFromAuth0UserMeMeMutationRegisterUser.Email, and is useful for accessing the field via an interface.
+func (v *CreateUserFromAuth0UserMeMeMutationRegisterUser) GetEmail() string { return v.Email }
+
+// GetAuth0UserId returns CreateUserFromAuth0UserMeMeMutationRegisterUser.Auth0UserId, and is useful for accessing the field via an interface.
+func (v *CreateUserFromAuth0UserMeMeMutationRegisterUser) GetAuth0UserId() string {
 	return v.Auth0UserId
 }
 
-// GetOrganization returns CreateUserFromAuth0UserCreateUserFromAuth0User.Organization, and is useful for accessing the field via an interface.
-func (v *CreateUserFromAuth0UserCreateUserFromAuth0User) GetOrganization() CreateUserFromAuth0UserCreateUserFromAuth0UserOrganization {
+// GetOrganization returns CreateUserFromAuth0UserMeMeMutationRegisterUser.Organization, and is useful for accessing the field via an interface.
+func (v *CreateUserFromAuth0UserMeMeMutationRegisterUser) GetOrganization() CreateUserFromAuth0UserMeMeMutationRegisterUserOrganization {
 	return v.Organization
 }
 
-// GetAuth0UserInfo returns CreateUserFromAuth0UserCreateUserFromAuth0User.Auth0UserInfo, and is useful for accessing the field via an interface.
-func (v *CreateUserFromAuth0UserCreateUserFromAuth0User) GetAuth0UserInfo() CreateUserFromAuth0UserCreateUserFromAuth0UserAuth0UserInfo {
+// GetAuth0UserInfo returns CreateUserFromAuth0UserMeMeMutationRegisterUser.Auth0UserInfo, and is useful for accessing the field via an interface.
+func (v *CreateUserFromAuth0UserMeMeMutationRegisterUser) GetAuth0UserInfo() CreateUserFromAuth0UserMeMeMutationRegisterUserAuth0UserInfo {
 	return v.Auth0UserInfo
 }
 
-// CreateUserFromAuth0UserCreateUserFromAuth0UserAuth0UserInfo includes the requested fields of the GraphQL type Auth0UserInfo.
-type CreateUserFromAuth0UserCreateUserFromAuth0UserAuth0UserInfo struct {
+// CreateUserFromAuth0UserMeMeMutationRegisterUserAuth0UserInfo includes the requested fields of the GraphQL type Auth0UserInfo.
+type CreateUserFromAuth0UserMeMeMutationRegisterUserAuth0UserInfo struct {
 	Name string `json:"name"`
 }
 
-// GetName returns CreateUserFromAuth0UserCreateUserFromAuth0UserAuth0UserInfo.Name, and is useful for accessing the field via an interface.
-func (v *CreateUserFromAuth0UserCreateUserFromAuth0UserAuth0UserInfo) GetName() string { return v.Name }
+// GetName returns CreateUserFromAuth0UserMeMeMutationRegisterUserAuth0UserInfo.Name, and is useful for accessing the field via an interface.
+func (v *CreateUserFromAuth0UserMeMeMutationRegisterUserAuth0UserInfo) GetName() string {
+	return v.Name
+}
 
-// CreateUserFromAuth0UserCreateUserFromAuth0UserOrganization includes the requested fields of the GraphQL type Organization.
-type CreateUserFromAuth0UserCreateUserFromAuth0UserOrganization struct {
+// CreateUserFromAuth0UserMeMeMutationRegisterUserOrganization includes the requested fields of the GraphQL type Organization.
+type CreateUserFromAuth0UserMeMeMutationRegisterUserOrganization struct {
 	Id string `json:"id"`
 }
 
-// GetId returns CreateUserFromAuth0UserCreateUserFromAuth0UserOrganization.Id, and is useful for accessing the field via an interface.
-func (v *CreateUserFromAuth0UserCreateUserFromAuth0UserOrganization) GetId() string { return v.Id }
+// GetId returns CreateUserFromAuth0UserMeMeMutationRegisterUserOrganization.Id, and is useful for accessing the field via an interface.
+func (v *CreateUserFromAuth0UserMeMeMutationRegisterUserOrganization) GetId() string { return v.Id }
 
 // CreateUserFromAuth0UserResponse is returned by CreateUserFromAuth0User on success.
 type CreateUserFromAuth0UserResponse struct {
-	CreateUserFromAuth0User CreateUserFromAuth0UserCreateUserFromAuth0User `json:"createUserFromAuth0User"`
+	Me CreateUserFromAuth0UserMeMeMutation `json:"me"`
 }
 
-// GetCreateUserFromAuth0User returns CreateUserFromAuth0UserResponse.CreateUserFromAuth0User, and is useful for accessing the field via an interface.
-func (v *CreateUserFromAuth0UserResponse) GetCreateUserFromAuth0User() CreateUserFromAuth0UserCreateUserFromAuth0User {
-	return v.CreateUserFromAuth0User
+// GetMe returns CreateUserFromAuth0UserResponse.Me, and is useful for accessing the field via an interface.
+func (v *CreateUserFromAuth0UserResponse) GetMe() CreateUserFromAuth0UserMeMeMutation { return v.Me }
+
+// GetUserByAuth0UserMe includes the requested fields of the GraphQL type Me.
+type GetUserByAuth0UserMe struct {
+	User GetUserByAuth0UserMeUser `json:"user"`
 }
 
-// GetUserByAuth0UserResponse is returned by GetUserByAuth0User on success.
-type GetUserByAuth0UserResponse struct {
-	UserByAuth0User GetUserByAuth0UserUserByAuth0User `json:"userByAuth0User"`
+// GetUser returns GetUserByAuth0UserMe.User, and is useful for accessing the field via an interface.
+func (v *GetUserByAuth0UserMe) GetUser() GetUserByAuth0UserMeUser { return v.User }
+
+// GetUserByAuth0UserMeUser includes the requested fields of the GraphQL type User.
+type GetUserByAuth0UserMeUser struct {
+	Id            string                                `json:"id"`
+	Email         string                                `json:"email"`
+	Auth0UserId   string                                `json:"auth0UserId"`
+	Organization  GetUserByAuth0UserMeUserOrganization  `json:"organization"`
+	Auth0UserInfo GetUserByAuth0UserMeUserAuth0UserInfo `json:"auth0UserInfo"`
 }
 
-// GetUserByAuth0User returns GetUserByAuth0UserResponse.UserByAuth0User, and is useful for accessing the field via an interface.
-func (v *GetUserByAuth0UserResponse) GetUserByAuth0User() GetUserByAuth0UserUserByAuth0User {
-	return v.UserByAuth0User
-}
+// GetId returns GetUserByAuth0UserMeUser.Id, and is useful for accessing the field via an interface.
+func (v *GetUserByAuth0UserMeUser) GetId() string { return v.Id }
 
-// GetUserByAuth0UserUserByAuth0User includes the requested fields of the GraphQL type User.
-type GetUserByAuth0UserUserByAuth0User struct {
-	Id            string                                         `json:"id"`
-	Email         string                                         `json:"email"`
-	Auth0UserId   string                                         `json:"auth0UserId"`
-	Organization  GetUserByAuth0UserUserByAuth0UserOrganization  `json:"organization"`
-	Auth0UserInfo GetUserByAuth0UserUserByAuth0UserAuth0UserInfo `json:"auth0UserInfo"`
-}
+// GetEmail returns GetUserByAuth0UserMeUser.Email, and is useful for accessing the field via an interface.
+func (v *GetUserByAuth0UserMeUser) GetEmail() string { return v.Email }
 
-// GetId returns GetUserByAuth0UserUserByAuth0User.Id, and is useful for accessing the field via an interface.
-func (v *GetUserByAuth0UserUserByAuth0User) GetId() string { return v.Id }
+// GetAuth0UserId returns GetUserByAuth0UserMeUser.Auth0UserId, and is useful for accessing the field via an interface.
+func (v *GetUserByAuth0UserMeUser) GetAuth0UserId() string { return v.Auth0UserId }
 
-// GetEmail returns GetUserByAuth0UserUserByAuth0User.Email, and is useful for accessing the field via an interface.
-func (v *GetUserByAuth0UserUserByAuth0User) GetEmail() string { return v.Email }
-
-// GetAuth0UserId returns GetUserByAuth0UserUserByAuth0User.Auth0UserId, and is useful for accessing the field via an interface.
-func (v *GetUserByAuth0UserUserByAuth0User) GetAuth0UserId() string { return v.Auth0UserId }
-
-// GetOrganization returns GetUserByAuth0UserUserByAuth0User.Organization, and is useful for accessing the field via an interface.
-func (v *GetUserByAuth0UserUserByAuth0User) GetOrganization() GetUserByAuth0UserUserByAuth0UserOrganization {
+// GetOrganization returns GetUserByAuth0UserMeUser.Organization, and is useful for accessing the field via an interface.
+func (v *GetUserByAuth0UserMeUser) GetOrganization() GetUserByAuth0UserMeUserOrganization {
 	return v.Organization
 }
 
-// GetAuth0UserInfo returns GetUserByAuth0UserUserByAuth0User.Auth0UserInfo, and is useful for accessing the field via an interface.
-func (v *GetUserByAuth0UserUserByAuth0User) GetAuth0UserInfo() GetUserByAuth0UserUserByAuth0UserAuth0UserInfo {
+// GetAuth0UserInfo returns GetUserByAuth0UserMeUser.Auth0UserInfo, and is useful for accessing the field via an interface.
+func (v *GetUserByAuth0UserMeUser) GetAuth0UserInfo() GetUserByAuth0UserMeUserAuth0UserInfo {
 	return v.Auth0UserInfo
 }
 
-// GetUserByAuth0UserUserByAuth0UserAuth0UserInfo includes the requested fields of the GraphQL type Auth0UserInfo.
-type GetUserByAuth0UserUserByAuth0UserAuth0UserInfo struct {
+// GetUserByAuth0UserMeUserAuth0UserInfo includes the requested fields of the GraphQL type Auth0UserInfo.
+type GetUserByAuth0UserMeUserAuth0UserInfo struct {
 	Name string `json:"name"`
 }
 
-// GetName returns GetUserByAuth0UserUserByAuth0UserAuth0UserInfo.Name, and is useful for accessing the field via an interface.
-func (v *GetUserByAuth0UserUserByAuth0UserAuth0UserInfo) GetName() string { return v.Name }
+// GetName returns GetUserByAuth0UserMeUserAuth0UserInfo.Name, and is useful for accessing the field via an interface.
+func (v *GetUserByAuth0UserMeUserAuth0UserInfo) GetName() string { return v.Name }
 
-// GetUserByAuth0UserUserByAuth0UserOrganization includes the requested fields of the GraphQL type Organization.
-type GetUserByAuth0UserUserByAuth0UserOrganization struct {
+// GetUserByAuth0UserMeUserOrganization includes the requested fields of the GraphQL type Organization.
+type GetUserByAuth0UserMeUserOrganization struct {
 	Id string `json:"id"`
 }
 
-// GetId returns GetUserByAuth0UserUserByAuth0UserOrganization.Id, and is useful for accessing the field via an interface.
-func (v *GetUserByAuth0UserUserByAuth0UserOrganization) GetId() string { return v.Id }
+// GetId returns GetUserByAuth0UserMeUserOrganization.Id, and is useful for accessing the field via an interface.
+func (v *GetUserByAuth0UserMeUserOrganization) GetId() string { return v.Id }
+
+// GetUserByAuth0UserResponse is returned by GetUserByAuth0User on success.
+type GetUserByAuth0UserResponse struct {
+	Me GetUserByAuth0UserMe `json:"me"`
+}
+
+// GetMe returns GetUserByAuth0UserResponse.Me, and is useful for accessing the field via an interface.
+func (v *GetUserByAuth0UserResponse) GetMe() GetUserByAuth0UserMe { return v.Me }
 
 func CreateUserFromAuth0User(
 	ctx context.Context,
@@ -126,15 +142,17 @@ func CreateUserFromAuth0User(
 		OpName: "CreateUserFromAuth0User",
 		Query: `
 mutation CreateUserFromAuth0User {
-	createUserFromAuth0User {
-		id
-		email
-		auth0UserId
-		organization {
+	me {
+		registerUser {
 			id
-		}
-		auth0UserInfo {
-			name
+			email
+			auth0UserId
+			organization {
+				id
+			}
+			auth0UserInfo {
+				name
+			}
 		}
 	}
 }
@@ -162,15 +180,17 @@ func GetUserByAuth0User(
 		OpName: "GetUserByAuth0User",
 		Query: `
 query GetUserByAuth0User {
-	userByAuth0User {
-		id
-		email
-		auth0UserId
-		organization {
+	me {
+		user {
 			id
-		}
-		auth0UserInfo {
-			name
+			email
+			auth0UserId
+			organization {
+				id
+			}
+			auth0UserInfo {
+				name
+			}
 		}
 	}
 }
