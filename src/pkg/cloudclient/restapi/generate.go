ea9d0b20469f79d697b163f6b3b6735e5600803b
Amit Lichtenberg <amitlicht@otterize.com>
2023-01-25 08:24:33 +0200

    silence curl

--- src/pkg/cloudclient/restapi/generate.go
+++ src/pkg/cloudclient/restapi/generate.go
@@ -1,4 +1,4 @@
 package restapi
 
-//go:generate curl -q http://local.otterize.com:4000/api/openapi.json -o ./cloudapi/openapi.json
+//go:generate curl -s http://local.otterize.com:4000/api/openapi.json -o ./cloudapi/openapi.json
 //go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen -o ./cloudapi/api.gen.go --package=cloudapi -generate=types,client ./cloudapi/openapi.json
