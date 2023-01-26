513a024600b2256959b341325167a6b4c56c5d11
Netanel Bollag <54136032+NetanelBollag@users.noreply.github.com>
2023-01-26 20:34:16 +0200

    Print services as table (#52)

--- src/cmd/services/list/list-services.go
+++ src/cmd/services/list/list-services.go
@@ -7,6 +7,7 @@ import (
 	"github.com/otterize/otterize-cli/src/pkg/cloudclient/restapi/cloudapi"
 	"github.com/otterize/otterize-cli/src/pkg/config"
 	"github.com/otterize/otterize-cli/src/pkg/output"
+	"github.com/otterize/otterize-cli/src/pkg/utils/prints"
 	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -19,10 +20,6 @@ var ListCmd = &cobra.Command{
 	Long:         ``,
 	SilenceUsage: true,
 	RunE:         listServices,
-}
-
-type servicesList struct {
-	Services []cloudapi.Service `json:"services"`
 }
 
 func listServices(_ *cobra.Command, _ []string) error {
@@ -52,11 +49,13 @@ func listServices(_ *cobra.Command, _ []string) error {
 
 	services := lo.FromPtr(resp.JSON200)
 
-	result, err := output.GetFormattedObject(servicesList{services})
+	columns := []string{"id", "name", "namespace", "env", "kafka info", "certificate info"}
+	result, err := output.FormatList(services, columns, serviceToColumns)
 	if err != nil {
 		return err
 	}
-	fmt.Println(result)
+
+	prints.PrintCliOutput(result)
 	return nil
 }
 
@@ -66,4 +65,64 @@ func init() {
 	ListCmd.Flags().String(ServiceNameKey, "", "filter list by service name")
 
 	ListCmd.Flags().String(config.OutputFormatKey, config.OutputFormatDefault, fmt.Sprintf("output format - %s/%s", config.OutputYaml, config.OutputJson))
+}
+
+func serviceToColumns(s cloudapi.Service) []map[string]string {
+	var kafkaInfo string
+	var certificateInfo string
+	if s.KafkaServerConfig != nil {
+		kafkaInfo = getKafkaInfo(*s.KafkaServerConfig)
+	}
+
+	if s.CertificateInformation != nil {
+		certificateInfo = getCertificateInformation(*s.CertificateInformation)
+	}
+
+	return []map[string]string{{
+		"id":               s.Id,
+		"name":             s.Name,
+		"namespace":        s.Namespace.Name,
+		"env":              s.Environment.Id,
+		"kafka info":       kafkaInfo,
+		"certificate info": certificateInfo,
+	}}
+}
+
+func getCertificateInformation(cert cloudapi.CertificateInformation) string {
+	var certificateInfo, commonName, dns, ttl string
+	commonName = cert.CommonName
+	if cert.DnsNames != nil {
+		for _, dnsName := range *cert.DnsNames {
+			dns += fmt.Sprintf("%s,", dnsName)
+		}
+	}
+	if cert.Ttl != nil {
+		ttl = fmt.Sprintf("%d", *cert.Ttl)
+	}
+
+	certificateInfo = fmt.Sprintf("common-name=%s,", commonName)
+	if len(dns) > 0 {
+		certificateInfo += fmt.Sprintf("dns-names=%s", dns)
+	}
+	if len(ttl) > 0 {
+		certificateInfo += fmt.Sprintf("ttl=%s,", ttl)
+	}
+	return certificateInfo
+}
+
+func getKafkaInfo(ksc cloudapi.KafkaServerConfig) string {
+	var kafkaInfo string
+	var topics string
+	for _, topic := range ksc.Topics {
+		topics += fmt.Sprintf("%s,pattern=%s,client-identity-required=%t,intents-required=%t,", topic.Topic, string(topic.Pattern), topic.ClientIdentityRequired, topic.IntentsRequired)
+	}
+	var address string
+	if ksc.Address != nil {
+		address = *ksc.Address
+		kafkaInfo = fmt.Sprintf("%s,", address)
+	}
+	if len(topics) > 0 {
+		kafkaInfo += fmt.Sprintf("topics=%s", topics)
+	}
+	return kafkaInfo
 }
