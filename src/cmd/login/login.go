fcf00862dbae7017ee132fab463dbacebd72f1e7
Amit Lichtenberg <amitlicht@otterize.com>
2023-01-29 10:43:11 +0200

    Touchup login documentation

--- src/cmd/login/login.go
+++ src/cmd/login/login.go
@@ -16,63 +16,61 @@ import (
 const SwitchAccountFlagKey = "switch-account"
 const SwitchOrgFlagKey = "switch-org"
 
-func login(_ *cobra.Command, _ []string) error {
-	getConfCtxTimeout, cancel := context.WithTimeout(context.Background(), config.DefaultTimeout)
-	defer cancel()
-
-	otterizeAPIAddress := viper.GetString(config.OtterizeAPIAddressKey)
-	authClient := auth_api.NewClient(otterizeAPIAddress)
-	auth0Config, err := authClient.GetAuth0Config(getConfCtxTimeout)
-	if err != nil {
-		return err
-	}
-
-	loginServer := server.NewLoginServer(auth0Config)
-	loginServer.Start()
-	loginUrl := loginServer.GetLoginUrl(viper.GetBool(SwitchAccountFlagKey))
-	if err := browser.OpenURL(loginUrl); err != nil {
-		logrus.Warning("Failed to open browser:", err)
-	}
-	prints.PrintCliStderr("Please login to Otterize using your browser: %s", loginUrl)
-	authResult := <-loginServer.GetAuthResultChannel()
-	prints.PrintCliStderr("Login completed successfully! logged in as: %s", authResult.Profile["email"])
-
-	apiAddress := viper.GetString(config.OtterizeAPIAddressKey)
-	loginCtx, err := userlogin.NewContext(apiAddress, authResult.AccessToken)
-	if err != nil {
-		return err
-	}
-
-	if err := loginCtx.EnsureUserRegistered(); err != nil {
-		return err
-	}
-
-	selectedOrgId, err := loginCtx.SelectOrg(viper.GetString(config.ApiSelectedOrganizationId), viper.GetBool(SwitchOrgFlagKey))
-	if err != nil {
-		return err
-	}
-
-	if err := config.SaveConfig(config.Config{
-		UserToken:      authResult.AccessToken,
-		OrganizationId: selectedOrgId,
-	}); err != nil {
-		return err
-	}
-
-	prints.PrintCliStderr("To change your organization selection, log-in again with --%s.", SwitchOrgFlagKey)
-	return nil
-}
-
 var LoginCmd = &cobra.Command{
 	Use:          "login",
-	Short:        "Login to Otterize using a web browser",
-	Long:         "",
-	RunE:         login,
+	Short:        "Login to Otterize Cloud",
+	Long:         ``,
 	Args:         cobra.NoArgs,
 	SilenceUsage: true,
+	RunE: func(_ *cobra.Command, _ []string) error {
+		getConfCtxTimeout, cancel := context.WithTimeout(context.Background(), config.DefaultTimeout)
+		defer cancel()
+
+		otterizeAPIAddress := viper.GetString(config.OtterizeAPIAddressKey)
+		authClient := auth_api.NewClient(otterizeAPIAddress)
+		auth0Config, err := authClient.GetAuth0Config(getConfCtxTimeout)
+		if err != nil {
+			return err
+		}
+
+		loginServer := server.NewLoginServer(auth0Config)
+		loginServer.Start()
+		loginUrl := loginServer.GetLoginUrl(viper.GetBool(SwitchAccountFlagKey))
+		if err := browser.OpenURL(loginUrl); err != nil {
+			logrus.Warning("Failed to open browser:", err)
+		}
+		prints.PrintCliStderr("Please login to Otterize using your browser: %s", loginUrl)
+		authResult := <-loginServer.GetAuthResultChannel()
+		prints.PrintCliStderr("Login completed successfully! logged in as: %s", authResult.Profile["email"])
+
+		apiAddress := viper.GetString(config.OtterizeAPIAddressKey)
+		loginCtx, err := userlogin.NewContext(apiAddress, authResult.AccessToken)
+		if err != nil {
+			return err
+		}
+
+		if err := loginCtx.EnsureUserRegistered(); err != nil {
+			return err
+		}
+
+		selectedOrgId, err := loginCtx.SelectOrg(viper.GetString(config.ApiSelectedOrganizationId), viper.GetBool(SwitchOrgFlagKey))
+		if err != nil {
+			return err
+		}
+
+		if err := config.SaveConfig(config.Config{
+			UserToken:      authResult.AccessToken,
+			OrganizationId: selectedOrgId,
+		}); err != nil {
+			return err
+		}
+
+		prints.PrintCliStderr("To change your organization selection, log-in again with --%s.", SwitchOrgFlagKey)
+		return nil
+	},
 }
 
 func init() {
-	LoginCmd.Flags().Bool(SwitchAccountFlagKey, false, "Switch to another account")
+	LoginCmd.Flags().Bool(SwitchAccountFlagKey, false, "Switch to a different user account")
 	LoginCmd.Flags().Bool(SwitchOrgFlagKey, false, "Switch to a different organization")
 }
