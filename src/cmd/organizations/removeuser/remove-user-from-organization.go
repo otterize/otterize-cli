858a84444aab10a32989480733d16144b80711f9
Amit Lichtenberg <amitlicht@otterize.com>
2023-01-29 15:27:07 +0200

    Add orgs remove-user command

---
+++ src/cmd/organizations/removeuser/remove-user-from-organization.go
@@ -0,0 +1,50 @@
+package removeuser
+
+import (
+	"context"
+	cloudclient "github.com/otterize/otterize-cli/src/pkg/cloudclient/restapi"
+	"github.com/otterize/otterize-cli/src/pkg/cloudclient/restapi/cloudapi"
+	"github.com/otterize/otterize-cli/src/pkg/config"
+	"github.com/otterize/otterize-cli/src/pkg/utils/prints"
+	"github.com/samber/lo"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const (
+	UserIDKey = "user-id"
+)
+
+var RemoveUserFromOrganizationCmd = &cobra.Command{
+	Use:          "remove-user <organization-id>",
+	Short:        "Update an organization",
+	Args:         cobra.ExactArgs(1),
+	SilenceUsage: true,
+	RunE: func(_ *cobra.Command, args []string) error {
+		ctxTimeout, cancel := context.WithTimeout(context.Background(), config.DefaultTimeout)
+		defer cancel()
+		c, err := cloudclient.NewClient(ctxTimeout)
+		if err != nil {
+			return err
+		}
+
+		id := args[0]
+		r, err := c.RemoveUserFromOrganizationMutationWithResponse(ctxTimeout,
+			id,
+			&cloudapi.RemoveUserFromOrganizationMutationParams{
+				UserId: viper.GetString(UserIDKey),
+			},
+		)
+		if err != nil {
+			return err
+		}
+
+		prints.PrintCliStderr("User %s removed from organization %s", lo.FromPtr(r.JSON200), id)
+		return nil
+	},
+}
+
+func init() {
+	RemoveUserFromOrganizationCmd.Flags().String(UserIDKey, "", "user id")
+	cobra.CheckErr(RemoveUserFromOrganizationCmd.MarkFlagRequired(UserIDKey))
+}
