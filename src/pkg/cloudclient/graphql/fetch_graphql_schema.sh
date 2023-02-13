d53d25f7429d1caecf59b55b738e36ea60170cfa
Amit Lichtenberg <amitlicht@otterize.com>
2023-02-13 20:23:58 +0200

    Fixes to generate

--- src/pkg/cloudclient/graphql/fetch_graphql_schema.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env sh
-gqlfetch --endpoint "http://local.otterize.com:4000/api/graphql/v1beta" > schema.graphql
--- src/pkg/cloudclient/graphql/generate.go
+++ src/pkg/cloudclient/graphql/generate.go
@@ -1,4 +1,6 @@
 package graphql
 
-//go:generate ./fetch_graphql_schema.sh
+import _ "github.com/suessflorian/gqlfetch"
+
+//go:generate sh -c "go run github.com/suessflorian/gqlfetch/gqlfetch --endpoint http://localhost:4000/api/graphql/v1beta > schema.graphql"
 //go:generate go run github.com/Khan/genqlient ./genqlient.yaml
