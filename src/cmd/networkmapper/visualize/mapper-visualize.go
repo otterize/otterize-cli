d9278ab128a53aeeff54e4a123ef82c2dd314cef
evyatarmeged <evyatar.m@otterize.com>
2023-02-14 17:10:35 +0200

    Final tweaks for graph visualize

--- src/cmd/networkmapper/visualize/mapper-visualize.go
+++ src/cmd/networkmapper/visualize/mapper-visualize.go
@@ -5,18 +5,25 @@ import (
 	"fmt"
 	"github.com/goccy/go-graphviz"
 	"github.com/goccy/go-graphviz/cgraph"
+	"github.com/otterize/otterize-cli/src/pkg/config"
 	"github.com/otterize/otterize-cli/src/pkg/mapperclient"
+	"github.com/otterize/otterize-cli/src/pkg/output"
 	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"strings"
 )
 
 const (
 	NamespacesKey       = "namespaces"
 	NamespacesShorthand = "n"
+	GraphFormatKey      = "graph-format"
 )
 
+var AllowedFormats = []string{"png", "dot", "svg", "jpg"}
+
 type Visualizer struct {
+	*graphviz.Graphviz
 	graph     *cgraph.Graph
 	nodeCache map[string]*cgraph.Node
 }
@@ -28,6 +35,7 @@ func NewVisualizer() *Visualizer {
 		panic(err)
 	}
 	return &Visualizer{
+		Graphviz:  g,
 		graph:     graph,
 		nodeCache: make(map[string]*cgraph.Node, 0),
 	}
@@ -60,13 +68,35 @@ func (v *Visualizer) populateNodeCache(serviceIntents []mapperclient.ServiceInte
 	return nil
 }
 
+func (v *Visualizer) buildEdges(serviceIntents []mapperclient.ServiceIntentsUpToMapperV017ServiceIntents) error {
+	for _, service := range serviceIntents {
+		clientNameWithNS := fmt.Sprintf("%s.%s", service.Client.Name, service.Client.Namespace)
+		for _, intent := range service.Intents {
+			targetNameWithNS := formatTargetServiceName(service.Client.Namespace, intent)
+			_, err := v.graph.CreateEdge(
+				fmt.Sprintf("%s to %s", clientNameWithNS, targetNameWithNS),
+				v.nodeCache[clientNameWithNS],
+				v.nodeCache[targetNameWithNS])
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
 var VisualizeCmd = &cobra.Command{
 	Use:   "visualize",
-	Short: "Visualize an access graph for network mapper intents",
+	Short: "Visualize an access graph for network mapper intents using go-graphviz",
 	Args:  cobra.NoArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return mapperclient.WithClient(func(c *mapperclient.Client) error {
 			namespacesFilter := viper.GetStringSlice(NamespacesKey)
+			format := viper.GetString(GraphFormatKey)
+			if err := validateOutputFormat(format); err != nil {
+				return err
+			}
+			outFile := viper.GetString(config.OutputPathKey)
 			servicesIntents, err := c.ServiceIntents(context.Background(), namespacesFilter)
 			if err != nil {
 				return err
@@ -76,18 +106,41 @@ var VisualizeCmd = &cobra.Command{
 				if err := visualizer.graph.Close(); err != nil {
 					panic(err)
 				}
+				if err := visualizer.Close(); err != nil {
+					panic(err)
+				}
 			}()
 			if err := visualizer.populateNodeCache(servicesIntents); err != nil {
 				return err
 			}
 
+			if err := visualizer.buildEdges(servicesIntents); err != nil {
+				return err
+			}
+
+			if err := visualizer.RenderFilename(visualizer.graph, graphviz.Format(format), outFile); err != nil {
+				return err
+			}
+
+			output.PrintStdout("Exported graph as %s format to path %s", format, outFile)
 			return nil
 		})
 	},
 }
 
+func validateOutputFormat(format string) error {
+	if !lo.Contains(AllowedFormats, strings.ToLower(format)) {
+		return fmt.Errorf("unsupported format: %s", format)
+	}
+
+	return nil
+}
+
 func init() {
 	VisualizeCmd.Flags().StringSliceP(NamespacesKey, NamespacesShorthand, nil, "filter for specific namespaces")
+	VisualizeCmd.Flags().String(GraphFormatKey, "svg", "Graph output format (png/svg/jpg/dot)")
+	VisualizeCmd.Flags().StringP(config.OutputPathKey, config.OutputPathShorthand, "", "filter for specific namespaces")
+	cobra.CheckErr(VisualizeCmd.MarkFlagRequired(config.OutputPathKey))
 }
 
 func formatTargetServiceName(clientNS string, target mapperclient.ServiceIntentsUpToMapperV017ServiceIntentsIntentsOtterizeServiceIdentity) string {
