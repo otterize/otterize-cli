568d5a56f4acb41c05ff7755502509399a19c05e
Ori Shoshan <ori@otterize.com>
2023-03-07 22:24:59 +0200

    Update API version

--- src/pkg/cloudclient/restapi/cloudapi/api.gen.go
+++ src/pkg/cloudclient/restapi/cloudapi/api.gen.go
@@ -22,7 +22,6 @@ const (
 	AccessTokenCookieScopes  = "accessTokenCookie.Scopes"
 	BearerAuthScopes         = "bearerAuth.Scopes"
 	Oauth2Scopes             = "oauth2.Scopes"
-	OrganizationCookieScopes = "organizationCookie.Scopes"
 	OrganizationHeaderScopes = "organizationHeader.Scopes"
 )
 
@@ -592,6 +591,7 @@ type CreateClusterMutationJSONBody struct {
 // UpdateClusterMutationJSONBody defines parameters for UpdateClusterMutation.
 type UpdateClusterMutationJSONBody struct {
 	Configuration *ClusterConfigurationInput `json:"configuration,omitempty"`
+	Name          *string                    `json:"name,omitempty"`
 }
 
 // OneEnvironmentQueryParams defines parameters for OneEnvironmentQuery.
